e00b85daaf4bda1bf335c53a670390f8bd846962
ant <tkan145@gmail.com>
2018-06-08 23:28:29 +1000

    Vote and Vote option migration files

--- database/migrations/2018_06_07_182257_create_options_table.php
+++ database/migrations/2018_06_07_182257_create_options_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateVotesTable extends Migration
+class CreateOptionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateVotesTable extends Migration
      */
     public function up()
     {
-        Schema::create('votes', function (Blueprint $table) {
+        Schema::create('options', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned();
+            $table->integer('vote_id')->unsigned();;
             $table->string('uuid')->unique();
-            $table->string('title');
-            $table->text('description');
-            $table->string('status');
-            $table->string('shareable_link')->unique();
+            $table->string('option');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateVotesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('votes');
+        Schema::dropIfExists('options');
     }
 }
